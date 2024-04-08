a3a8f9a4deae4034d5ebbbd52afddb049d0efa62
alberto <alberto.taddeucci@gmail.com>
2024-04-08 16:27:49 +0200

    inserimento lista comics in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,41 @@
 
 @section('content')
 
-{{-- content here --}}
+<main id="home">
+    <div id="jumbo"></div>
+    <div class="container">
+        
+    </div>
+    <div class="comics-container">
+
+        <div class="container">
+
+            <div class="comics">
+                <div class="current">
+                    <span>CURRENT SERIES</span>
+                </div>
+        
+                @foreach ($comics as $comicItem)
+                    
+                <div class="comic">
+                    <div>
+                        <img src="{{$comicItem['thumb']}}" alt="">
+                    </div>
+                    <span>
+                        {{ $comicItem['title'] }}
+                    </span>
+    
+                </div>
+                @endforeach
+                               
+        
+            </div>
+        </div>
+    
+        <button>Load More</button>
+    </div>
+    
+</main>
+
 
 @endsection
